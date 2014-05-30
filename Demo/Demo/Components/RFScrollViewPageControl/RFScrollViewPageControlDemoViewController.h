a0df94a7051fe70bf1df2b01b5fbc3985cff7abf
BB9z <bb9z@me.com>
2014-05-30 11:59:24 +0800

    Rewrite RFScrollViewPageControlDemo.

--- Demo/Demo/Components/RFScrollViewPageControl/RFScrollViewPageControlDemoViewController.h
+++ Demo/Demo/Components/RFScrollViewPageControl/RFScrollViewPageControlDemoViewController.h
@@ -9,17 +9,21 @@
 #import "RFUI.h"
 #import "RFScrollViewPageControl.h"
 
-@interface RFScrollViewPageControlDemoViewController : UIViewController
-<UIScrollViewDelegate>
+@class SimplePageScrollView;
 
-@property(weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@interface RFScrollViewPageControlDemoViewController : UIViewController
+@property(weak, nonatomic) IBOutlet SimplePageScrollView *scrollView;
 @property(weak, nonatomic) IBOutlet RFScrollViewPageControl *pageControl;
 
 // Config
-@property(assign, nonatomic) float pageCount;
 @property(weak, nonatomic) IBOutlet UILabel *pageCountLabel;
 @property(weak, nonatomic) IBOutlet UISlider *pageCountSlider;
 
 - (IBAction)onPageCountSliderValueChanged:(UISlider *)sender;
 - (IBAction)onSupportHalfPageSwitchChanged:(UISwitch *)sender;
+@end
+
+
+@interface SimplePageScrollView : UIScrollView
+@property (assign, nonatomic) float pageCount;
 @end
--- Demo/Demo/Components/RFScrollViewPageControl/RFScrollViewPageControlDemoViewController.m
+++ Demo/Demo/Components/RFScrollViewPageControl/RFScrollViewPageControlDemoViewController.m
@@ -2,83 +2,86 @@
 #import "RFScrollViewPageControlDemoViewController.h"
 #import "UIView+RFAnimate.h"
 
-@interface RFScrollViewPageControlDemoViewController ()
-@end
-
 @implementation RFScrollViewPageControlDemoViewController
-RFUIInterfaceOrientationSupportAll
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-}
-
-#pragma mark - Config page count
+RFUIInterfaceOrientationSupportPortrait
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [self.scrollView removeAllSubviews];
-    [self onPageCountSliderValueChanged:self.pageCountSlider];
 
-    self.scrollView.translatesAutoresizingMaskIntoConstraints = YES;
-}
-
-- (void)viewDidLayoutSubviews {
-    [super viewDidLayoutSubviews];
-    
-    if (self.pageCount < 0) return;
-    
-    CGFloat width = self.scrollView.width;
-    CGFloat height = self.scrollView.height;
-    NSUInteger subviewCountCurrent = self.scrollView.subviews.count;
-    NSUInteger subviewCountWillBe = ceilf(self.pageCount);
-    
-    UIView *lastSubview = [self.scrollView.subviews lastObject];
-    if (subviewCountWillBe > subviewCountCurrent) {
-        lastSubview.width = width;
-        
-        for (NSInteger i = (subviewCountWillBe - subviewCountCurrent); i > 0; i--) {
-            UIView *v = [[UIView alloc] initWithFrame:CGRectMake((subviewCountWillBe - i)*width, 10, width, height-20)];
-            v.autoresizingMask = UIViewAutoresizingFlexibleSize | UIViewAutoresizingFlexibleMargin;
-            v.translatesAutoresizingMaskIntoConstraints = YES;
-            v.backgroundColor = [UIColor randColorWithAlpha:0.8];
-            [self.scrollView addSubview:v];
-            _dout(@"Add a view: %@", v)
-        }
-    }
-    else {
-        // Remove last n views.
-        [self.scrollView.subviews enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(UIView * view, NSUInteger idx, BOOL *stop) {
-            if (idx >= subviewCountWillBe) {
-                [view removeFromSuperview];
-            }
-            else {
-                *stop = YES;
-            }
-        }];
-    }
-    
-    // Fix last view size.
-    CGFloat lastWidth = width * (self.pageCount - floorf(self.pageCount));
-    [[self.scrollView.subviews lastObject] setWidth:(lastWidth)? : width];
-    
-    // Adjust x
-    [self.scrollView.subviews enumerateObjectsWithOptions:NSEnumerationConcurrent usingBlock:^(UIView * view, NSUInteger idx, BOOL *stop) {
-        view.x = idx*width;
-    }];
-    
-    _douto(self.scrollView.subviews)
-    self.scrollView.contentSize = (CGSize){ self.pageCount * self.scrollView.bounds.size.width, 0 };
+    self.scrollView.pageCount = self.pageCountSlider.value;
 }
 
 - (IBAction)onPageCountSliderValueChanged:(UISlider *)sender {
     self.pageCountLabel.text = [NSString stringWithFormat:@"%.1f", sender.value];
-    self.pageCount = sender.value;
-    [self.view setNeedsLayout];
 }
 
 - (IBAction)onSupportHalfPageSwitchChanged:(UISwitch *)sender {
     self.pageControl.supportHalfPage = sender.on;
+}
+
+@end
+
+
+@implementation SimplePageScrollView
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self removeAllSubviews];
+}
+
+- (void)setPageCount:(float)pageCount {
+    RFAssert(pageCount >= 0, @"Page count can not be negative");
+
+    if (_pageCount != pageCount) {
+        CGFloat width = self.width;
+        CGFloat height = self.height;
+
+        NSUInteger subviewCountCurrent = _pageCount;
+        NSUInteger subviewCountWillBe = ceilf(pageCount);
+        if (subviewCountWillBe > subviewCountCurrent) {
+            for (NSInteger i = (subviewCountWillBe - subviewCountCurrent); i > 0; i--) {
+                UIView *v = [[UIView alloc] initWithFrame:CGRectMake((subviewCountWillBe - i)*width, 10, width, height-20)];
+                v.autoresizingMask = UIViewAutoresizingFlexibleSize | UIViewAutoresizingFlexibleMargin;
+                v.backgroundColor = [UIColor randColorWithAlpha:0.8];
+                [self addSubview:v];
+                dout(@"Add a view: %@", v)
+            }
+        }
+        else {
+            // Remove last n views.
+            [self.subviews enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(UIView * view, NSUInteger idx, BOOL *stop) {
+                if (idx >= subviewCountWillBe) {
+                    [view removeFromSuperview];
+                }
+                else {
+                    *stop = YES;
+                }
+            }];
+        }
+
+        _pageCount = pageCount;
+
+        [self setNeedsLayout];
+    }
+}
+
+- (void)layoutSubviews {
+    // No super
+
+    CGFloat width = self.width;
+    CGFloat height = self.height;
+
+    // Fix last view size.
+    CGFloat lastWidth = width * (self.pageCount - floorf(self.pageCount));
+    [[self.subviews lastObject] setWidth:(lastWidth)? : width];
+
+    // Adjust x
+    [self.subviews enumerateObjectsWithOptions:NSEnumerationConcurrent usingBlock:^(UIView * view, NSUInteger idx, BOOL *stop) {
+        view.frame = CGRectMake(idx * width, 10, width, height - 20);
+    }];
+
+    _douto(self.subviews)
+    self.contentSize = (CGSize){ self.pageCount * width, 0 };
 }
 
 @end
