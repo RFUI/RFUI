fb3ed5d014435212670b0d0002ce8815440d2c59
BB9z <bb9z@me.com>
2014-09-30 10:41:20 +0800

    New demo for RFImageCropper.

---
+++ Demo/Demo/Components/RFImageCropper/RFDImageCropperViewController.h
@@ -0,0 +1,14 @@
+//
+//  RFDImageCropperViewController.h
+//  RFDemo
+//
+//  Created by BB9z on 9/30/14.
+//  Copyright (c) 2014 RFUI. All rights reserved.
+//
+
+#import "RFUI.h"
+#import "RFImageCropperView.h"
+
+@interface RFDImageCropperViewController : UIViewController
+@property (weak, nonatomic) IBOutlet RFImageCropperView *imageCropper;
+@end
---
+++ Demo/Demo/Components/RFImageCropper/RFDImageCropperViewController.m
@@ -0,0 +1,60 @@
+
+#import "RFDImageCropperViewController.h"
+
+@interface RFDImageCropperViewController () <
+    UIImagePickerControllerDelegate
+>
+@end
+
+@implementation RFDImageCropperViewController
+RFUIInterfaceOrientationSupportAll
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.imageCropper.cropSize = CGSizeMake(300, 300);
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.navigationController setToolbarHidden:NO animated:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.navigationController setToolbarHidden:YES animated:animated];
+}
+
+- (IBAction)onPickPhoto:(id)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = (id)self;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    self.imageCropper.sourceImage = image;
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)onCropSizeChangeTo1x1:(id)sender {
+    [UIView animateWithDuration:.3 animations:^{
+        self.imageCropper.cropSize = CGSizeMake(300, 300);
+    }];
+}
+
+- (IBAction)onCropSizeChangeTo3x4:(id)sender {
+    [UIView animateWithDuration:.3 animations:^{
+        self.imageCropper.cropSize = CGSizeMake(300, 400);
+    }];
+}
+
+- (IBAction)onCropSizeChangeTo4x3:(id)sender {
+    [UIView animateWithDuration:.3 animations:^{
+        self.imageCropper.cropSize = CGSizeMake(300, 225);
+    }];
+}
+
+- (IBAction)onCrop:(id)sender {
+}
+
+@end
